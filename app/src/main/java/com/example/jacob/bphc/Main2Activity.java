914240025e6fdd9dfb72a560e6040c1b80390f54
Zachary Safran <zsafran@bu.edu>
2016-04-02 16:47:37 -0500

    really fixed
    the real fix

--- app/src/main/java/com/example/jacob/bphc/Main2Activity.java
+++ app/src/main/java/com/example/jacob/bphc/Main2Activity.java
@@ -1,36 +1,28 @@
 package com.example.jacob.bphc;
 
-<<<<<<< HEAD
-=======
+
 import android.content.Intent;
->>>>>>> origin/master
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-<<<<<<< HEAD
-
-public class Main2Activity extends AppCompatActivity {
-
-=======
 import android.widget.Button;
+
 
 public class Main2Activity extends AppCompatActivity {
 
     private Button mButton3;
     private Button mButton4;
 
->>>>>>> origin/master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-<<<<<<< HEAD
-=======
+
 
         mButton3 = (Button) findViewById(R.id.button3);
         mButton4 = (Button) findViewById(R.id.button4);
@@ -58,7 +50,7 @@ public class Main2Activity extends AppCompatActivity {
     private void startForum(){
         Intent intent = new Intent(this, ForumActivity.class);
         startActivity(intent);
->>>>>>> origin/master
+
     }
 
 }
